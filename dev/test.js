763d3cfff1c82c20009bc442aa7dfb78202461ea
David <davidrums65@gmail.com>
2019-12-01 14:07:21 +0100

    Second Commit

---
+++ dev/test.js
@@ -0,0 +1,14 @@
+const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(2389, 'siondndoi8932973', 'hujeuohei9829832');
+
+bitcoin.createNewTransaction(100, 'alexklfnioeuwiu32', 'jenjfn932989089');
+bitcoin.createNewBlock(1928, '1092091029sd2cds', 'kjaskhb8723dddsd7');
+
+bitcoin.createNewTransaction(120, 'kjnkjnkdndkns', 'slkalkslkalkjlksj');
+
+bitcoin.createNewTransaction(200, 'djndjnkdnsdff', 'dijdosijodijo');
+bitcoin.createNewBlock(33233, 'sdpkodpoidpokds', 'dkjdjdjnknddd');
+
+console.log(bitcoin.chain[2]);
